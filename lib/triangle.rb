501a0530388224944bc7343d821785e2a2fff711
Aarow Epstein <aarowepstein1@gmail.com>
2023-06-22 17:11:47 -0700

    commit tree a5847c4eeeddc500c9cbe8556d59f4e34ad8e275

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @sides.sum < 0 || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a || @sides.any?{|i| i < 0}
+      raise TriangleError
+      elsif @a == @b && @a == @c && @b == @c
+        return :equilateral
+      elsif @a == @b || @a == @c || @b == @c
+        return :isosceles
+      else
+        return :scalene
+      end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Please enter a valid triangle"
+    end
+  end
 end
+
